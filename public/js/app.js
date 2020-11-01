3a77e39690225dcad0e99175e52fa5b4e4f0fe4b
y-ash2001 <f20191026@pilani.bits-pilani.ac.in>
2020-11-01 11:36:09 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,20 +6,18 @@ const message2 = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const location = search.value;
-	fetch('http://localhost:3000/weather?address=' + location).then(
-		(response) => {
-			response.json().then((data) => {
-				if (data.error) {
-					message1.textContent = data.error;
-				} else {
-					message1.textContent = 'Location: ' + data.location;
-					message2.textContent =
-						'Description: ' +
-						data.forecastdata.description +
-						' | Temperature: ' +
-						data.forecastdata.temperature;
-				}
-			});
-		}
-	);
+	fetch('/weather?address=' + location).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				message1.textContent = data.error;
+			} else {
+				message1.textContent = 'Location: ' + data.location;
+				message2.textContent =
+					'Description: ' +
+					data.forecastdata.description +
+					' | Temperature: ' +
+					data.forecastdata.temperature;
+			}
+		});
+	});
 });
